64ad22c3c0f16c6e2ae270093e9ef6e747ce2a4d
kesara <kesrabandara1998@gmail.com>
2023-09-16 13:25:22 +0530

    config

--- src/test/java/com/thejobs/onlineappointmentschedulingwebsite/ServiceTestConfig.java
+++ src/test/java/com/thejobs/onlineappointmentschedulingwebsite/ServiceTestConfig.java
@@ -1,14 +1,12 @@
 package com.thejobs.onlineappointmentschedulingwebsite;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.thejobs.onlineappointmentschedulingwebsite.service.AuthenticationService;
 
 @Configuration
-public class ConsultantServiceTestConfig {
+public class ServiceTestConfig {
 
     @MockBean
     private AuthenticationService authenticationService;
 
-    // You can add more mock beans if needed
 }
